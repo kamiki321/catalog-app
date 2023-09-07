7ca073b4f79c64c41e66a7b88d7cf1aee88c4ffb
kamiki321 <82932748+kamiki321@users.noreply.github.com>
2023-09-07 13:35:13 +0700

    2 files added

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+function Navbar(){
+  return(
+    <header>
+      <h3>Logo</h3>
+      <nav>
+        <a href="/#">Dashboard</a>
+        <a href="/#">Search Catalog</a>
+        <a href="/#">Home</a>
+      </nav>
+    </header>
+  )
+}
